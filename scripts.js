3e4051db0a41c782d0868ef3fd9c656fcaf9aa1f
shamill72 <75394386+shamill72@users.noreply.github.com>
2024-08-05 11:46:01 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -45,4 +45,13 @@ $(document).ready(function() {
             }
         });
     }
+        function botTrap() {
+       let botCheck = document.getElementById('gotcha').value;
+       console.log(botCheck);
+       if(botCheck !== '') {
+        console.log('Bot detected');
+        return false;
+       }
+       return true;
+    }
 });
